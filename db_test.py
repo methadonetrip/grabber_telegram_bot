f7ae8ba9c39fb9e13eaf5844a4f4badc4221d775
shaaarin <mascunok@gmail.com>
2023-12-22 00:42:32 +0300

    DataBase Tests

---
+++ db_test.py
@@ -0,0 +1,32 @@
+import pytest
+from unittest.mock import Mock, patch
+from db import add_user_to_db
+
+@pytest.mark.asyncio
+async def test_add_user_to_db():
+    mock_connection = Mock()
+    mock_cursor = Mock()
+    sql = "INSERT INTO userid (id) VALUES (%s) ON DUPLICATE KEY UPDATE id = id"
+    chat_id = 1122
+
+    with patch('db.add_user_to_db', mock_connection):
+
+        await add_user_to_db(chat_id)
+
+    assert mock_cursor.execute(sql, (chat_id))
+
+    assert mock_connection.commit()
+
+@pytest.mark.asyncio
+async def test_add_user_to_db_exception():
+    mock_connection = Mock()
+    mock_cursor = Mock()
+    err = '''Error adding user to database: (1366, "Incorrect integer value: '' for column 'id' at row 1")'''
+    chat_id=''
+
+
+    with pytest.raises(Exception) as ex:
+        assert add_user_to_db(chat_id) == err
+
+    mock_cursor.execute.assert_not_called()
+    mock_connection.commit.assert_not_called()
