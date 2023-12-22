4ea4d13d44110e7cb57a5b820a0bb1356bbc636a
shaaarin <mascunok@gmail.com>
2023-12-22 12:47:31 +0300

    DataBase final tests

--- db_test.py
+++ db_test.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import Mock, patch
-from db import add_user_to_db
+from db import add_user_to_db, add_user_status
 
 @pytest.mark.asyncio
 async def test_add_user_to_db():
@@ -10,11 +10,9 @@ async def test_add_user_to_db():
     chat_id = 1122
 
     with patch('db.add_user_to_db', mock_connection):
-
         await add_user_to_db(chat_id)
 
     assert mock_cursor.execute(sql, (chat_id))
-
     assert mock_connection.commit()
 
 @pytest.mark.asyncio
@@ -22,13 +20,45 @@ async def test_add_user_to_db_exception():
     mock_connection = Mock()
     mock_cursor = Mock()
     err = '''Error adding user to database: (1366, "Incorrect integer value: '' for column 'id' at row 1")'''
-    chat_id=''
+    chat_id = ''
 
     with patch('db.add_user_to_db', mock_connection):
         await add_user_to_db(chat_id)
 
     with pytest.raises(Exception) as ex:
         await add_user_to_db(chat_id)
+        assert err in str(ex.value)
+
+    mock_cursor.execute.assert_not_called()
+    mock_connection.commit.assert_not_called()
+
+@pytest.mark.asyncio
+async def test_add_user_status():
+    mock_connection = Mock()
+    mock_cursor = Mock()
+    sql = "UPDATE userid SET status = %s WHERE id = %s "
+    chat_id = 1122
+    state = 1
+
+    with patch('db.add_user_to_db', mock_connection):
+        await add_user_status(state, chat_id)
+
+    assert mock_cursor.execute(sql, (state, chat_id))
+    assert mock_connection.commit()
+
+@pytest.mark.asyncio
+async def test_add_user_status_exception():
+    mock_connection = Mock()
+    mock_cursor = Mock()
+    err = '''Error adding user to database: (1366, "Incorrect integer value: '' for column 'id' at row 1")'''
+    chat_id = ''
+    state = ''
+
+    with patch('db.add_user_to_db', mock_connection):
+        await add_user_status(state, chat_id)
+
+    with pytest.raises(Exception) as ex:
+        await add_user_status(state, chat_id)
         assert err in str(ex.value)
 
     mock_cursor.execute.assert_not_called()
