bc3ad833b83bf1e985e91bd76bb8644b7b187c71
m30wm30www1amak1tten <97727534+methadonetrip@users.noreply.github.com>
2024-01-26 01:45:03 +0300

    update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #MySQL
-host = "127.0.0.1"
+host = "ur host"
 user = "root"
-password = "Po25qwe!"
+password = "ur password"
 db_name = "usersinfo"
 
 #API
-api_id = '27149831'
-api_hash = '0f2f5320a88e77d90f2cd470ce8353aa' 
-bot_token = '6776409787:AAEOHAIOpKMdTK9dFhQVJRLB09c3lqd5-HQ'
+api_id = 'ur id'
+api_hash = 'ur hash' 
+bot_token = 'ur token'
